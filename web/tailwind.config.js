8b124517b16ae1e474e26f3b16c10787bf0b64a0
Tamiraa Sanjaajav <tamirasanjaa@gmail.com>
2022-09-02 23:12:10 +0800

    tailwind config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-purple': '#7A81FF',
+        'primary-blue':'#5259FF',
+        'dark-text':'#334354'
+      },
+      fontFamily: {
+        'sans': ['Work Sans']
+      }
+    },
   },
   plugins: [],
 };
