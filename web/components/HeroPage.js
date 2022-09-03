e9e26c83599fc3ce3096f7a640d3f3607cf621c5
Tamiraa Sanjaajav <tamirasanjaa@gmail.com>
2022-09-03 18:05:42 +0800

    hero page

---
+++ web/components/HeroPage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import YouTube from 'react-youtube';
+
+const HeroPage = ({hex1, hex2, bigText, smallText, url}) => {
+    
+    console.log(hex1, hex2, url, bigText);
+  return (
+    <center><div className={`h-4/5 bg-gradient-to-br from-primary-purple to-pale-pink w-3/4 justify-between flex-row snap-center p-[40px] rounded-3xl rounded-br-[2.4rem]`}>
+        <div>
+          <h1 className='text-[2.6rem]'>{bigText}</h1>
+          <p className='text-[1.3rem]'>{smallText}</p>
+        </div>
+        <YouTube videoId={url}/>
+    </div></center>
+    
+  )
+}
+
+export default HeroPage
--- web/pages/index.js
+++ web/pages/index.js
@@ -1,10 +1,20 @@
 import { gql, GraphQLClient } from "graphql-request";
-
+import Navbar from '../components/Navbar';
+import HeroPage from "../components/HeroPage";
 import React from "react";
 
 const Home = ({ heroPage }) => {
   console.log(heroPage);
-  return <h1 className="text-3xl font-bold">Hello world!</h1>;
+  const hex1 = heroPage?.gradientColor1.hex;
+  const hex2 = heroPage?.gradientColor2.hex;
+  const bigText = heroPage?.header[0].bigText;
+  const smallText = heroPage?.header[0].smallText;
+  const urlIndex = heroPage?.youtubeLink.url.lastIndexOf('/');
+  const vidID = heroPage?.youtubeLink.url.slice(urlIndex);
+  return <div className="w-full h-screen font-sans">
+    {/* <Navbar/> */}
+    <HeroPage hex1={hex1} hex2={hex2} bigText={bigText} smallText={smallText} url={vidID}/>
+  </div>
 };
 
 export default Home;
